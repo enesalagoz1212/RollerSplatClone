256add7e998fc0f730d47196a75259cac1cb2fb4
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2023-12-14 19:46:41 +0300

    jumpAnimation create

--- Assets/_GameFolder/Scripts/Controllers/BallController.cs
+++ Assets/_GameFolder/Scripts/Controllers/BallController.cs
@@ -35,6 +35,10 @@ namespace RollerSplatClone.Controllers
 		public float moveDuration;
 		public Ease move;
 
+		private float jumpDelay = 10f;
+		private float lastInputTime;
+		private bool isJumping = false;
+		private Tween jumpTween;
 		public void Initialize(LevelManager levelManager)
 		{
 			_levelManager = levelManager;
@@ -63,22 +67,27 @@ namespace RollerSplatClone.Controllers
 
 		public void OnScreenDrag(Direction direction)
 		{
-            if (!_canMove)
+			if (!_canMove)
 			{
-                return;
+				return;
 			}
 
-            var targetGroundController = _levelManager.ReturnDirectionGroundController(direction, _groundController);
+			var targetGroundController = _levelManager.ReturnDirectionGroundController(direction, _groundController);
 
 			if (targetGroundController != null)
 			{
-                _canMove = false;
+				if (isJumping)
+				{
+					jumpTween.Kill();
+					isJumping = false;
+				}
+				_canMove = false;
 				var targetPosition = targetGroundController.transform.position;
 				transform.DOMove(targetPosition, moveDuration).SetEase(move).OnComplete(() =>
 				{
 					_groundController = targetGroundController;
 					_canMove = true;
-
+					lastInputTime = Time.time;
 				});
 			}
 			else
@@ -86,7 +95,47 @@ namespace RollerSplatClone.Controllers
 				Debug.Log($"Can not move!");
 			}
 		}
-		
+
+		private void Update()
+		{
+			if (GameManager.Instance.GameState == GameState.Playing)
+			{
+
+				JumpAnimation();
+				if (Input.GetMouseButtonDown(0) && isJumping)
+				{
+					KillJumpAnimation();
+					ReturnToOriginalPosition();
+					lastInputTime = Time.time;
+				}
+			}
+		}
+
+		private void ReturnToOriginalPosition()
+		{
+			transform.DOMove(_groundController.transform.position, moveDuration).SetEase(move);
+		}
+
+		private void JumpAnimation()
+		{
+			if (Time.time - lastInputTime > jumpDelay && !isJumping)
+			{
+				var jumpHeight = 1.5f;
+				isJumping = true;
+
+				jumpTween = transform.DOJump(transform.position, jumpHeight, 1, 0.7f).SetLoops(-1, LoopType.Restart);
+			}
+		}
+
+		public void KillJumpAnimation()
+		{
+			if (jumpTween != null && jumpTween.IsActive())
+			{
+				isJumping = false;
+				jumpTween.Kill();
+			}
+		}
+
 		public void ColorTheBall(Color color)
 		{
 			ballRenderer.material.color = color;
